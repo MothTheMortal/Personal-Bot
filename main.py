72516a1d66f3c59bf40eb3b8e34a4df80e4a8ea4
MothTheMortal <mohdmotha687@gmail.com>
2023-08-28 07:17:15 +0800

    Auto assign of Bot role to bots

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from os import getenv
 from datetime import datetime
+
 load_dotenv()
 
 TOKEN = getenv("BOT_TOKEN")
@@ -10,6 +11,7 @@ TOKEN = getenv("BOT_TOKEN")
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix="?!")
 
 color_theme = 0x2fd034
+
 
 @bot.event
 async def on_ready():
@@ -19,8 +21,12 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member: discord.Member):
-    member_role = member.guild.get_role(1145461173718896731)
-    await member.add_roles(member_role, reason="New Member Role!")
+    if member.bot:
+        bot_role = member.guild.get_role(1145465466203152515)
+        await member.add_roles(bot_role, reason="New Bot role!")
+    else:
+        member_role = member.guild.get_role(1145461173718896731)
+        await member.add_roles(member_role, reason="New Member Role!")
 
 
 @bot.tree.command(name="make-suggestion", description="Give a suggestion for a feature to add to the bot.")
@@ -32,8 +38,7 @@ async def make_suggestion(ctx: discord.Interaction, feature_summary: str, featur
 
     past_message = [message async for message in suggestion_channel.history(limit=1)][0]
     title = past_message.embeds[0].title
-    counter = int(title[1:title.find(" ")+1]) + 1
-
+    counter = int(title[1:title.find(" ") + 1]) + 1
 
     embed = discord.Embed(title=f"#{counter} " + feature_summary[:254], description=feature[:4096], color=color_theme)
     embed.timestamp = datetime.now()
