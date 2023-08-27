664ba4a31f03c384d1f6ba328de3caca934b486b
MothTheMortal <mohdmotha687@gmail.com>
2023-08-28 05:43:01 +0800

    Temporary Functionality for the Bot

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+TOKEN = getenv("BOT_TOKEN")
+
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix="?!")
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} is online!")
+    print(f"{len(await bot.tree.sync())} commands loaded!")
+
+
+@bot.event
+async def on_member_join(member: discord.Member):
+    member_role = member.guild.get_role(1145461173718896731)
+    await member.add_roles(member_role, reason="New Member Role!")
+
+
+bot.run(TOKEN)
+
+
