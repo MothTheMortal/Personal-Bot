40ddd5a9d5cbccd5866212c87632cf2199d4d40c
MothTheMortal <mohdmotha687@gmail.com>
2023-08-30 02:10:41 +0800

    Added Youtube Downloader command

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import os
-
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 from os import getenv
 from datetime import datetime
 from discord.app_commands import Choice, choices
+from discord import app_commands
 from gtts import gTTS
 import io
 import asyncio
+import pytube
+
 load_dotenv()
 
 tts_lock = asyncio.Lock()
@@ -36,12 +38,43 @@ async def on_member_join(member: discord.Member):
         await member.add_roles(member_role, reason="New Member Role!")
 
 
+@bot.tree.command(name="download-yt", description="Download a Youtube Audio/Video")
+@choices(filetype=[Choice(name="Audio Only - MP3", value="MP3"), Choice(name="Video & Audio - MP4", value="MP4")])
+async def download_yt(ctx: discord.Interaction, link: str, filetype: Choice[str]):
+    yt = pytube.YouTube(link)
+
+    description = f"Title: {yt.title}\nLength: {yt.length // 60} minutes {yt.length % 60} seconds"
+
+    embed = discord.Embed(title="Youtube Downloader", description=description)
+    embed.set_image(url=yt.thumbnail_url)
+    embed.set_footer(text="Downloading...")
+
+    await ctx.response.send_message(embed=embed)
+
+    if filetype.value == "MP3":
+        stream = yt.streams.get_audio_only()
+    else:
+        stream = yt.streams.get_highest_resolution()
+
+    video_data = io.BytesIO()
+    stream.stream_to_buffer(video_data)
+    video_data.seek(0)
+
+    file = discord.File(video_data, filename=f"{title[:32]}.mp4")
+
+    embed.set_footer(text="Downloaded.")
+
+    await ctx.edit_original_response(embed=embed)
+    msg = await ctx.original_response()
+    await msg.reply(file=file)
+
+
 @bot.command(name="ttsa")
 async def ttsa(ctx: commands.Context, language, *, text=None):
     if tts_lock.locked():
         await ctx.send("The command is already running. Please wait.")
         return
-    
+
     async with tts_lock:
         channel = ctx.author.voice.channel
 
@@ -67,7 +100,6 @@ async def ttsa(ctx: commands.Context, language, *, text=None):
 
 @bot.command(name="tts")
 async def tts(ctx: commands.Context, *, text=None):
-
     if tts_lock.locked():
         await ctx.send("The command is already running. Please wait.")
         return
@@ -80,10 +112,8 @@ async def tts(ctx: commands.Context, *, text=None):
 
         voice_client = await channel.connect()
 
-
         audio = gTTS(text, lang="en")
         audio.save("ttsaudio.mp3")
-
 
         source = discord.FFmpegPCMAudio("ttsaudio.mp3")
         voice_client.play(source)
@@ -96,7 +126,8 @@ async def tts(ctx: commands.Context, *, text=None):
 @bot.tree.command(name="introduce-yourself", description="Command to introduce yourself.")
 @choices(gender=[Choice(name="Male", value="male"), Choice(name="Female", value="female")])
 @discord.app_commands.describe(birthday="DD/MM/YYYY Format.")
-async def introduce_yourself(ctx: discord.Interaction, name: str, gender: Choice[str], age: int, birthday: str, nationality: str, games: str, hobbies: str, languages: str, anything_note: str):
+async def introduce_yourself(ctx: discord.Interaction, name: str, gender: Choice[str], age: int, birthday: str,
+                             nationality: str, games: str, hobbies: str, languages: str, anything_note: str):
     intro_channel = ctx.guild.get_channel(1145593789130473573)
     names = [msg.embeds[0].author.name async for msg in intro_channel.history(limit=1000)]
 
@@ -112,7 +143,6 @@ async def introduce_yourself(ctx: discord.Interaction, name: str, gender: Choice
     await intro_channel.send(embed=embed)
 
     await ctx.response.send_message("Introduction sent!", ephemeral=True)
-
 
 
 @bot.tree.command(name="make-suggestion", description="Give a suggestion for a feature to add to the bot.")
