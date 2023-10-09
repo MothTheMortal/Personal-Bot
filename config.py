9bf1e5837dfd2368a8198f7de6daad465cfa59a5
MothTheMortal <mohdmotha687@gmail.com>
2023-10-09 14:05:51 +0800

    Update config.py

--- config.py
+++ config.py
@@ -347,9 +347,9 @@ def getRandomEye(H):
     R, G, B = int(R), int(G), int(B)
 
     Eye = Image.open("sphealeye.png")
-
-    ImageDraw.floodfill(Eye, EyeColorLeft, (R, G, B), thresh=50)
-    ImageDraw.floodfill(Eye, EyeColorRight, (R, G, B), thresh=50)
+    EyeColor = R, G, B
+    ImageDraw.floodfill(Eye, EyeColorLeft, EyeColor, thresh=50)
+    ImageDraw.floodfill(Eye, EyeColorRight, EyeColor, thresh=50)
 
     return Eye
 
