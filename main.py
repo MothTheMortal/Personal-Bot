4e2c708bcacc570fd35d0e553777267e346dea05
MothTheMortal <mohdmotha687@gmail.com>
2024-01-13 21:09:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -434,14 +434,14 @@ async def cat(ctx: commands.Context):
 
 
 @bot.command(name="snipe")
-async def snipe(ctx: commands.Context):
+async def snipe(ctx: commands.Context, count=0):
     snipe_channel = [channel for channel in ctx.guild.channels if channel.name == snipeName]
     if snipe_channel:
         snipe_channel = snipe_channel[0]
     else:
         return await ctx.send(f"channel `{snipeName}` not found!")
 
-    msg = [msg async for msg in snipe_channel.history()][0]
+    msg = [msg async for msg in snipe_channel.history()][count]
     if msg.attachments:
         await ctx.reply(files=[await f.to_file() for f in msg.attachments])
     else:
